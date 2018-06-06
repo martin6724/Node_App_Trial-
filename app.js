60ea2f2200e5e3cdb82bdcb8ea2ee753c24d3de5
martin6724 <martin.michelleann@gmail.com>
2018-06-06 11:33:59 -0400

    node app

---
+++ app.js
@@ -0,0 +1,27 @@
+// function greeting(greet){
+//   console.log(greet);
+// }
+//
+// greeting("hello world!");
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const fs = require('fs');
+
+fs.readFile('index.html', (err, html) => {
+  if(err){
+    throw err;
+  }
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/html');
+  res.write(html);
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log('Server started on port ' +port);
+});
+
+});
